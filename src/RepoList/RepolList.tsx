369bb4bc8d33d37316ffa4a5b370b4cc8f813c4c
Nishanth Singh Gurung <nishanthsinghgurung@Nishanths-MacBook-Pro.local>
2021-05-01 17:48:28 +0100

    fetching data from graphql api

--- src/RepoList/RepolList.tsx
+++ src/RepoList/RepolList.tsx
@@ -1,9 +1,84 @@
-import React from 'react';
-import { gql } from "@apollo/client";
+import React, { useEffect, useState } from 'react';
+import { gql, useQuery } from "@apollo/client";
+
+type Node = {
+    forkCount: number;
+    url: string;
+    stargazerCount: number;
+};
+
+type Edge = {
+    node: Node
+};
+
+const QUERY_REPO_LIST = gql`
+        query($first: Int!, $after: String, $query: String!, $type: SearchType!) {
+            search(first: $first, after:$after, query: $query, type: $type) {
+            edges {
+                node {
+                    ... on Repository {
+                        url
+                        forkCount
+                        stargazerCount
+                    }
+                }
+            }
+            pageInfo {
+                startCursor
+                hasNextPage
+                endCursor
+            }
+            }
+        }
+    `;
 
 const RepoList: React.FC  = () => {
+    const [repos, setRepos] = useState([] as Array<Node>);
+    const [numberOfReposPerPage, setNumberOfReposPerPage] = useState(10);
+    const [startCursor, setStartCursor] = useState(null);
+    const [endCursor, setEndCursor] = useState(null);
+
+    const { loading, error, data} = useQuery(QUERY_REPO_LIST, {
+        variables: {
+            "first": numberOfReposPerPage,
+            "after": endCursor,
+            "query": "react",
+            "type": "REPOSITORY"
+        }
+    });
+   
+    useEffect(() => {
+        if(data && data.search && data.search.edges) {
+            let repoInfo: Array<Node> = [];
+            data.search.edges.forEach(( { node } : Edge) => {
+                repoInfo.push({
+                    url: node? node.url: '',
+                    forkCount: node? node.forkCount: 0,
+                    stargazerCount: node? node.stargazerCount: 0
+                });
+            });
+            setRepos(repoInfo);
+        }
+    
+    }, [data])
+    
+    if(loading) {
+        return <div>Loading...</div>;
+    }
+    if(error) {
+        return <div>Error...</div>;
+    }
     return (
-        <div>Repo list</div>
+        <>
+            <header>Github Repo List</header>
+            <main>
+                {repos && repos.map(repo => (
+                    <div key={repo.url}>
+                        {repo.url}
+                    </div>
+                ))}
+            </main>
+        </>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = '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';
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
