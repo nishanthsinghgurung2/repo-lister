2e656284736d5486ab72b856d9ac52696a57317b
Nishanth Singh Gurung <nishanthsinghgurung@Nishanths-MacBook-Pro.local>
2021-05-01 15:00:54 +0100

    added docker config to build and run unit tests

---
+++ src/RepoList/RepolList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { gql } from "@apollo/client";
+
+const RepoList: React.FC  = () => {
+    return (
+        <div>Repo list</div>
+    );
+};
+
+export default RepoList;
